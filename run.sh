84d4c55e49f580f0bc4228899f6d5d2cc5a1515a
Bruno Almeida <brunoslalmeida@gmail.com>
2020-03-08 23:57:48 -0300

    fixing ssl

--- run.sh
+++ run.sh
@@ -11,17 +11,17 @@ if test -z "$CERTBOT_SSL"; then
 fi
 
 docker run -d \
-        -p 80:80 \
-        -p 443:443 \
-        --hostname nginx \
-        --name nginx \
-        --restart=always \
-        --network internal \
-        -v $CERTBOT_SSL:/etc/ssl:ro \
-        -v $cwd/var/log/nginx:/var/log/nginx \
-        -v $cwd/etc/conf.d/:/etc/nginx/conf.d:ro \
-        -v $cwd/etc/common.d/:/etc/nginx/common.d:ro \
-        -v $cwd/etc/default.d/:/etc/nginx/default.d:ro \
-        -v $cwd/etc/nginx.conf:/etc/nginx/nginx.conf:ro \
-        -v $cwd/usr/share/nginx/html:/usr/share/nginx/html:ro \
-        nginx
+  -p 80:80 \
+  -p 443:443 \
+  --hostname nginx \
+  --name nginx \
+  --restart=always \
+  --network internal \
+  -v $CERTBOT_SSL:/var/lib/letsencrypt:ro \
+  -v $cwd/var/log/nginx:/var/log/nginx \
+  -v $cwd/etc/conf.d/:/etc/nginx/conf.d:ro \
+  -v $cwd/etc/common.d/:/etc/nginx/common.d:ro \
+  -v $cwd/etc/default.d/:/etc/nginx/default.d:ro \
+  -v $cwd/etc/nginx.conf:/etc/nginx/nginx.conf:ro \
+  -v $cwd/usr/share/nginx/html:/usr/share/nginx/html:ro \
+  nginx
